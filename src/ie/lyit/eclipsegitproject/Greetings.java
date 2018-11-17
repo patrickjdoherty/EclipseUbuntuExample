934c616d086c912f1d1d642b2e74f4a0aaaf728d
patrickjdoherty <L00120294@student.lyit.ie>
2018-11-17 18:10:50 +0000

    Created greeting.java

---
+++ src/ie/lyit/eclipsegitproject/Greetings.java
@@ -0,0 +1,11 @@
+package ie.lyit.eclipsegitproject;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Greetings LYIT Student!");
+
+	}
+
+}

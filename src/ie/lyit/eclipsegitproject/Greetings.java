184b6479e68c89108558fdbf2a3bb09b8cb36e21
patrickjdoherty <L00120294@student.lyit.ie>
2018-11-17 18:44:24 +0000

    Added a second greeting.

--- src/ie/lyit/eclipsegitproject/Greetings.java
+++ src/ie/lyit/eclipsegitproject/Greetings.java
@@ -5,6 +5,8 @@ public class Greetings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Greetings LYIT Student!");
+		System.out.println("Greetings again LYIT Student!");
+
 
 	}
 
